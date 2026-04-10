3338a1096c3b870a497c14dceed0a69af0eb2888
Lenhu1102 <Lenhu1944@gmail.com>
2026-04-10 12:42:20 +0700

    Init project: add Book, Library, Main with input and display features

--- HeThongThuVien/src/Book.java
+++ HeThongThuVien/src/Book.java
@@ -1,37 +1,22 @@
 public class Book {
-    private String id;
     private String title;
     private String author;
-    private int quantity;
+    private int numberOfPages;
 
-    public Book (String bookid, String title, String author, int quantity){
-    this.id = bookid;
+    public Book (String title, String author, int numberOfPages){
     this.title = title;
     this.author = author;
-    this.quantity = quantity;
+    this.numberOfPages = numberOfPages;
 }
-public String getBookId(){
-    return id;
-}
-public String getTitle(){
+
+    public String getTitle(){
     return title;
 }
-public int getQuantity(){
-    return quantity;
-}
-public String getAuthor(){
-        return author;
-}
-public void borrowBook(){
-    if(quantity > 0){
-        quantity--;
-    }
-}
-public void returnBook(){
-    quantity ++;
-}
-public void display(){
-    System.out.println("Book_id: " + id + "."+ " Tên sách: " + title +"."+ " Tên tác giả: " +author+"."+ " Số lượng: " + quantity);
+    public String getAuthor(){ return author;}
+    public int getNumberOfPages(){return numberOfPages;}
+
+    public void display(){
+    System.out.println( "Tên sách: " + title +"."+ " Tên tác giả: " +author+"."+ " Số trang: " + numberOfPages);
     }
 }
 
--- HeThongThuVien/src/Main.java
+++ HeThongThuVien/src/Main.java
@@ -1,75 +1,36 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main (String[] args){
+    public static void main (String[] args) {
         //Tao thu vien
         Library library = new Library();
 
         //Tạo sách
-        Book b1 = new Book("B1","A","Nho ",1);
-        Book b2 = new Book("B2","B","Nho ",2);
-
-        //Tạo member
-        Member m1 = new Member("M1", "Nhu", 1,"");
-        Member m2 = new Member("M2","Nhi",2,"");
+        Book b1 = new Book("B1", "A", 200);
+        Book b2 = new Book("B2", "B", 100);
 
         //Add sách vào thư viện
         library.addBook(b1);
         library.addBook(b2);
 
-        //Add member
-        library.addMember(m1);
-        library.addMember(m2);
-
-        library.showBooks();
-
         //Search and Borrow book
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Nhập vào tên memberID");
-        String memberID = scanner.nextLine();
+        System.out.println("Nhập vào tên sách");
+        String title = scanner.nextLine();
 
-        System.out.println("Nhập vào tên sách cần mượn: ");
-        String bookTitle = scanner.nextLine();
+        System.out.println("Nhập vào tên tác giả: ");
+        String author = scanner.nextLine();
 
-        Book book = library.findBookByTitle(bookTitle);
-        Member member = library.findMember(memberID);
+        System.out.println("Nhập vào số trang: ");
+        int numberOfPages = scanner.nextInt();
 
-        if (member == null){
-            System.out.println("Member not found!");
-        }
-        else if (book == null)
-        {
-            System.out.println("Book not found!!");
-        } else if (book.getQuantity() == 0)
-        {
-            System.out.println("Book is out of stock!");
-        } else
-        {
-            library.borrowBook(memberID, book.getBookId());
-        }
+        Book book = new Book(title, author,numberOfPages);
+        library.addBook(book);
 
-        //Return book
-        System.out.print("Enter book ID to return: ");
-        String returnBookId = scanner.nextLine();
-
-        System.out.print("Enter member ID: ");
-        String returnMemberId = scanner.nextLine();
-
-        //check member tồn tại
-        Member returnMember = library.findMember(returnMemberId);
-
-        if (returnMember == null){
-            System.out.println("Member not found!");
-        } else {
-            library.returnBook(returnMemberId, returnBookId);
-        }
-
+        System.out.println("Tất cả sách trong kho gồm:");
+        library.showBooks();
         scanner.close();
-    }
-
-        //Show list final books
-        //System.out.println("Danh sách sách còn lại trong kho");
-       // library.showBooks();
 
     }
+}
